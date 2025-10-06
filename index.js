f4ea1e2404009cfc9d3abfb5a15678eb1dd0dbfb
Umar-Atif <umaratifweb@gmail.com>
2025-10-06 12:34:57 +0500

    changes in index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get('/', (req, res) => {
 
 // Middlewares
 app.use(express.json());
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
